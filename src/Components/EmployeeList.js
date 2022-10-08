8f0cf2bee7f43c91c6c976d7a7d586b6d82dd828
mohamedfazilk <mohamedfazil961@gmail.com>
2022-10-07 23:00:17 -0700

    create a component folder and place employee

---
+++ src/Components/EmployeeList.js
@@ -0,0 +1,10 @@
+import React,{useState} from 'react'
+
+const EmployeeList = () => {
+    const [employee, setEmployee] = useState([])
+  return (
+    <div>EmployeeList</div>
+  )
+}
+
+export default EmployeeList
