3734ab259ab5c81b9c36620b1b74e1f8f9bc0eae
mohamedfazilk <mohamedfazil961@gmail.com>
2022-10-17 16:08:40 -0700

    place the backend url and the id as url parameter for the onDelclick function

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ function App() {
         </Typography>
       </Toolbar>
     </AppBar>
-    <EmployeeList/>
+    <EmployeeList 
+    />
   </div>
   );
 }
--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,44 +1,83 @@
 import React, { useState, useEffect } from 'react'
+import { SERVER_URL } from '../Constants.js';
+import { DataGrid } from '@mui/x-data-grid';
+import Snackbar from '@mui/material/Snackbar';
 
 
-const url = 'http://localhost:8080/api/v1/employees'
 
 const EmployeeList = () => {
     const [employee, setEmployee] = useState([]);
+    const [open, setOpen] = useState(false);
+
 
     const fetchInfo = async () => {
+        try{
+            const response = await fetch(SERVER_URL + 'api/v1/employees');
+            const employee = await response.json()
+            setEmployee(employee);
+            console.log(employee);
+        }
+        catch(error){
+            console.log(error);
+        }
 
-        const response = await fetch(url);
-        const employee = await response.json()
-        setEmployee(employee);
-        console.log(employee);
-
-
+     
     }
+
 
     useEffect(() => {
         fetchInfo();
+        
     }, [])
 
+
+    const onDelClick = (url) => {
+        if (window.confirm("Are you sure to delete?"))
+        fetch(SERVER_URL + 'api/v1/employees/'+url, { method: 'DELETE'}, )
+            .then(res => {
+                fetchInfo();
+                setOpen(true)
+             
+            }).catch(err => console.log(err))
+        }
+
+    const columns = [
+        { field: 'firstName', headerName: 'First Name', width: 200 },
+        { field: 'lastName', headerName: 'Last Name', width: 200 },
+        { field: 'email', headerName: 'Email', width: 200 },
+        {
+            field: 'id',
+            headerName: '',
+            sortable: false,
+            filterable: false,
+            renderCell: row => <button onClick={() => onDelClick(row.id)}>Delete
+            </button>
+        }
+    ];
+
+   
+
+
+
+
+
+
     return (
-        <div>
-            <table>
-                <tbody>
-                    {
-                        employee.map((emp, index) => {
-                            return (
-                               <tr>
-                                <td>{emp.firstName}</td>
-                                <td>{emp.lastName}</td>
-                                <td>{emp.email}</td>
-                               </tr>
+        <div style={{ height: 500, width: '100%' }}>
 
-                            )
+            <DataGrid
+                rows={employee}
+                columns={columns}
+                disableSelectionOnClick={true}
+                getRowId={row => row.id} />
 
-                        })
-                    }
-                </tbody>
-            </table>
+            <Snackbar
+                open={open}
+                autoHideDuration={2000}
+                onClose={() => setOpen(false)}
+                message="Car deleted"
+            />
+
         </div>
     )
 }
